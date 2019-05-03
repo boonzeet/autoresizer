04589246411afd1c89c2d4f1d670da4685a3c218
Marc Lequime <mlequime@appriss.com>
2019-05-03 13:52:27 +0100

    Added support for custom scale factor, interpolation modes

--- bin/demo-global.js
+++ bin/demo-global.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
 var myLib = require('../lib/index.js');
 var args = process.argv.slice(2);
-// if custom extension supplied, call function with it
-if (args.length > 0 && typeof args[0] === "string") {
-    myLib.watch(process.cwd(), args[0])
-} else {
-    myLib.watch(process.cwd());
-}
+myLib.watch(process.cwd(), args);
